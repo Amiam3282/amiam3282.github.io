a999b8c362a12ab6bdbc0fe42bdfaf1ee96124ff
Amiam3282 <56267047+Amiam3282@users.noreply.github.com>
2020-10-15 10:53:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Amia M"
+let userName = "Amiam3282";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
